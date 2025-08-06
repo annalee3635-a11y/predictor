7bc4336aadc62b31be268450127906b81d31c712
Andy Lee <annalee3635@gmail.com>
2025-08-06 15:30:26 -0400

    added and fixed more starter code

--- predictor.py
+++ predictor.py
@@ -7,7 +7,8 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
 
 # Fetch historical stock data for Tesla
-stock_data = yf.download('TSLA', start='2016-10-01', end='2025-08-01')
+end_date = '2025-08-01'
+stock_data = yf.download('TSLA', start='2016-10-01', end=end_date)
 
 # Display the first few rows of the dataset
 # stock_data.head()
@@ -56,7 +57,7 @@ model.add(Dense(units=1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Train the model
-model.fit(x_train, y_train, epochs=20, batch_size=32)
+model.fit(x_train, y_train, epochs=5, batch_size=32)
 
 # Predict stock prices on the test data
 predictions = model.predict(x_test)
@@ -67,6 +68,22 @@ predictions = scaler.inverse_transform(predictions)
 # Inverse transform the actual test data
 y_test_scaled = scaler.inverse_transform(y_test.reshape(-1, 1))
 
+future_dates = pd.date_range(start=end_date, periods=seq_length)
+reshape_prices = close_prices.reshape(-1, 1)
+last_prices = reshape_prices[-seq_length:]
+last_prices_scaled = scaler.transform(last_prices.reshape(-1, 1))
+#x_pred = np.array([last_prices_scaled[-seq_length:, 0]])
+x_pred = last_prices_scaled
+x_pred = np.reshape(x_pred, (x_pred.shape[0], x_pred.shape[1], 1))
+predicted_prices_scaled = model.predict(x_pred)
+predicted_prices = scaler.inverse_transform(predicted_prices_scaled)
+
+print(predicted_prices)
+print(len(future_dates))
+print(len(predicted_prices.flatten()))
+future_data = pd.DataFrame({'Date': future_dates, 'Predicted Price': predicted_prices.flatten()})
+print(future_data)
+"""
 # Create a plotly figure
 fig = go.Figure()
 
@@ -81,3 +98,4 @@ fig.update_layout(title='Tesla Stock Price Prediction', xaxis_title='Date', yaxi
 
 # Show the figure
 fig.show()
+"""
