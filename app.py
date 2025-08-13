f46e6564e30b6a227f10eb972e264618915f8305
Andy Lee <annalee3635@gmail.com>
2025-08-13 13:08:55 -0400

    app starter code imported and edited

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import predictor
+
+app = Flask(__name__)
+app.config["DEBUG"] = False
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return """<h1>Stock Predictor</h1>
+              <p>This site predicts stock prices using an LSTM deep-learning model.
+              </p>"""
+
+
+@app.route("/api/predictor", methods=["GET"])
+def predict():
+    
+    # check if letters were provided. if so, assign it to variable
+    # if not, display an error
+
+    if 'ticker' in request.args:
+        ticker = request.args['ticker']
+    else:
+        return "Error: No ticker provided. Please provide stock ticker (eg. AAPL)."
+    
+    predictor.predict(ticker)
+
+    return "Your data will open in a new window"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
