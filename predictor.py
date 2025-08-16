ece24442edaae7471b9457b7f71a325f2efa077d
Andy Lee <annalee3635@gmail.com>
2025-08-16 19:45:23 -0400

    image doesn't load but doesn't crash

--- predictor.py
+++ predictor.py
@@ -21,8 +21,10 @@ def display(tckr):
     buf = BytesIO()
     fig.savefig(buf, format="png")
     # Embed the result in the html output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return render_template("predictionGraphs.html", **data)
+    data_for_passing = {
+        'data' : base64.b64encode(buf.getbuffer()).decode("ascii")
+    }
+    return render_template("predictionGraphs.html", **data_for_passing)
 
 @bp.route("/", methods=["GET", "POST"])
 def pred():
